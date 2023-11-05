b93c64d57944b8ed5ac7bfa1eda5112a67756732
michelmarquardt <56347550+michelmarquardt@users.noreply.github.com>
2023-11-05 03:40:59 +0100

    added skills section

--- portfolio-website/src/app/service/routing-service.service.ts
+++ portfolio-website/src/app/service/routing-service.service.ts
@@ -7,23 +7,7 @@ export class RoutingService {
 
   constructor() { }
 
-  toHome() {
-    document.getElementById("home")?.scrollIntoView({behavior: "smooth"});
-  }
-
-  toAbout() {
-    document.getElementById("about")?.scrollIntoView({behavior: "smooth"});
-  }
-
-  toSkills() {
-    document.getElementById("skills")?.scrollIntoView({behavior: "smooth"});
-  }
-
-  toEducation() {
-    document.getElementById("education")?.scrollIntoView({behavior: "smooth"});
-  }
-
-  toContact() {
-    document.getElementById("contact")?.scrollIntoView({behavior: "smooth"});
+  scrollTo(section: string) {
+    document.getElementById(section)?.scrollIntoView({behavior: 'smooth'});
   }
 }
--- portfolio-website/src/app/skills/skills.component.ts
+++ portfolio-website/src/app/skills/skills.component.ts
@@ -5,6 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss']
 })
+
+
 export class SkillsComponent {
+
+  skills: any[] = [
+    { name: 'HTML', src: '../../assets/skills/HTML5.png' },
+    { name: 'CSS', src: '../../assets/skills/CSS3.png' },
+    { name: 'Javascript', src: '../../assets/skills/JavaScript.png' },
+    { name: 'Typescript', src: '../../assets/skills/TypeScript.png' },
+    { name: 'Angluar', src: '../../assets/skills/Angluar.png' },
+    { name: 'React Native', src: '../../assets/skills/React_Native.png' },
+    { name: 'Vue', src: '../../assets/skills/Vue.png' },
+    { name: 'Flutter', src: '../../assets/skills/Flutter.png' },
+    { name: 'Dart', src: '../../assets/skills/Dart.png' },
+    { name: 'Java', src: '../../assets/skills/Java.png' },
+    { name: 'SQL', src: '../../assets/skills/SQL.png' },
+  ];
 
 }
